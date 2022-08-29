2352770663f8fd3723226e7647c7ed6f48e27c1c
franq01 <carreone705@gmail.com>
2022-08-29 16:16:35 -0500

    Reorganizacion de templates

--- web_personal/app.py
+++ web_personal/app.py
@@ -6,20 +6,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('public/index.html')
 
 @app.route('/about')
 def about():
-    return render_template ('about.html')
+    return render_template ('public/about.html')
 
 @app.route('/contact')
 def contact():
-    return render_template ('contact.html')
+    return render_template ('public/contact.html')
 
 
 @app.route('/portfolio')
 def portfolio():
-    return render_template ('portfolio.html')
+    return render_template ('public/portfolio.html')
 
 
 if __name__ == '__main__':
